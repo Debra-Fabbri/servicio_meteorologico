b0e83e3dac34f19f13b5142f5d1ecfc9a5380665
Debra Fabbri <debrafabbri@gmail.com>
2022-04-30 20:22:08 -0300

    Operadores avanzados & Librerias

--- index.js
+++ index.js
@@ -35,13 +35,13 @@ if (!ubicacion == true) {
 se va a repetir hasta que sean "exactamente Iguales", entonces sale de ese bucle y vemos que entramos o accedimos 
 a la pagina con su informacion"*/
 
-function validarUbicacion(ubicacion, volverUbicacion){
-    
-    while(ubicacion !== volverUbicacion){
+function validarUbicacion(ubicacion, volverUbicacion) {
 
-            alert("Ups! No ha ingresado la misma ubicación, por favor vuelva a intentarlo");
-            ubicacion = prompt("Hola! Por favor, Ingrese una provincia dentro de Argentina."); 
-            volverUbicacion = prompt("Por favor, confirme nuevamente la provincia ingresada.");
+    while (ubicacion !== volverUbicacion) {
+
+        alert("Ups! No ha ingresado la misma ubicación, por favor vuelva a intentarlo");
+        ubicacion = prompt("Hola! Por favor, Ingrese una provincia dentro de Argentina.");
+        volverUbicacion = prompt("Por favor, confirme nuevamente la provincia ingresada.");
     }
 
 }
@@ -53,8 +53,8 @@ validarUbicacion(ubicacion, volverUbicacion);
 //Luego creo una clase Provincia, que contiene un "constructor" donde le paso parametros que luego voy a utilizar en un Array 
 
 
-class provincias{
-    constructor(nombre, estado, temperatura, sensacionTermica, humedad, velocidadDelViento, presionDelAire, visibilidad, indiceUV){
+class provincias {
+    constructor(nombre, estado, temperatura, sensacionTermica, humedad, velocidadDelViento, presionDelAire, visibilidad, indiceUV) {
         this.nombre = nombre;
         this.estado = estado;
         this.temperatura = parseFloat(temperatura);
@@ -69,30 +69,30 @@ class provincias{
 
 //Array para Provincias, donde cada posicion es un objeto con sus atributos
 
-let provinciasArgentinas = [
-    new provincias("Buenos Aires","Lluvioso", 22, 22, 76, 11, 1.021, 5, "Bajo, 2"),
+const provinciasArgentinas = [
+    new provincias("Buenos Aires", "Lluvioso", 22, 22, 76, 11, 1.021, 5, "Bajo, 2"),
     new provincias("Ciudad Autonoma de Buenos Aires", "Tormenta Electrica", 15, 16, 83, 19, 1.006, 10, "Moderado, 4"),
-    new provincias("Catamarca","Soleado", 23, 23, 49, 37, 1.017, 16, "Moderado, 4"),
-    new provincias("Chaco","Soleado", 23, 23, 48, 13, 1.020, 16, "Moderado, 3"),
-    new provincias("Chubut","Soleado", 22, 23, 22, 19, 1.010, 16, "Bajo, 2"),
-    new provincias("Cordoba","Soleado", 25, 26, 60, 3, 1.021, 10, "Alto, 6"),
-    new provincias("Corrientes","Soleado", 23, 23, 46, 11, 1.020, 10, "Moderado, 4"),
-    new provincias("Entre Rios","Tormenta Electrica", 15, 16, 91, 18, 1.006, 10, "Bajo, 2"),
-    new provincias("Formosa","Soleado", 22, 22, 48, 18, 1.019, 10, "Bajo, 2"),
-    new provincias("Jujuy","Nublado",16, 16, 91, 8,  1.019, 10, "Bajo, 2"),
-    new provincias("La Pampa","Soleado", 23, 23, 30, 19, 1.019, 16, "Bajo, 2"),
-    new provincias("La Rioja","Parcialmente nublado", 23, 23, 59, 11, 1.016, 16,"Moderado, 3" ),
-    new provincias("Mendoza","Mayormente soleado", 23, 23, 31, 3, 1.016, 14, "Moderado, 3"),
+    new provincias("Catamarca", "Soleado", 23, 23, 49, 37, 1.017, 16, "Moderado, 4"),
+    new provincias("Chaco", "Soleado", 23, 23, 48, 13, 1.020, 16, "Moderado, 3"),
+    new provincias("Chubut", "Soleado", 22, 23, 22, 19, 1.010, 16, "Bajo, 2"),
+    new provincias("Cordoba", "Soleado", 25, 26, 60, 3, 1.021, 10, "Alto, 6"),
+    new provincias("Corrientes", "Soleado", 23, 23, 46, 11, 1.020, 10, "Moderado, 4"),
+    new provincias("Entre Rios", "Tormenta Electrica", 15, 16, 91, 18, 1.006, 10, "Bajo, 2"),
+    new provincias("Formosa", "Soleado", 22, 22, 48, 18, 1.019, 10, "Bajo, 2"),
+    new provincias("Jujuy", "Nublado", 16, 16, 91, 8, 1.019, 10, "Bajo, 2"),
+    new provincias("La Pampa", "Soleado", 23, 23, 30, 19, 1.019, 16, "Bajo, 2"),
+    new provincias("La Rioja", "Parcialmente nublado", 23, 23, 59, 11, 1.016, 16, "Moderado, 3"),
+    new provincias("Mendoza", "Mayormente soleado", 23, 23, 31, 3, 1.016, 14, "Moderado, 3"),
     new provincias("Misiones", "Soleado", 20, 20, 55, 14, 1.017, 14, "Bajo, 2"),
     new provincias("Neuquen", "Mayormente soleado", 24, 24, 26, 18, 1.016, 10, "Bajo, 2"),
     new provincias("Rio Negro", "Soleado", 22, 22, 27, 19, 1.016, 16, "Bajo, 1"),
-    new provincias("Salta","Nublado",17, 17, 72, 13,  1.022, 10, "Bajo, 1"),
-    new provincias("San Juan", "Mayormente soleado", 26, 26, 27, 10, 1.015, 14, "Moderado, 3"),
+    new provincias("Salta", "Nublado", 17, 17, 72, 13, 1.022, 10, "Bajo, 1"),
+    new provincias("San Juan", "Mayormente Soleado", 26, 26, 27, 10, 1.015, 14, "Moderado, 3"),
     new provincias("San Luis", "Mayormente soleado", 24, 24, 27, 11, 1.017, 16, "Bajo, 2"),
-    new provincias("Santa Cruz","Mayormente nublado", 19, 19, 45, 21, 1.005, 16, "Bajo, 1"),
-    new provincias("Santa Fe","Lluvioso", 16, 16, 87, 3, 1.007 , 10, "Bajo, 2"),
-    new provincias("Santiago del Estero","Mayormente soleado", 22.4, 22, 31, 4, 1.018, 10, "Moderado, 4"),
-    new provincias("Tierra del Fuego","Parcialmente nublado", 11, 8, 55, 31, 999, 14, "Bajo, 1"),
+    new provincias("Santa Cruz", "Mayormente nublado", 19, 19, 45, 21, 1.005, 16, "Bajo, 1"),
+    new provincias("Santa Fe", "Lluvioso", 16, 16, 87, 3, 1.007, 10, "Bajo, 2"),
+    new provincias("Santiago del Estero", "Mayormente soleado", 22.4, 22, 31, 4, 1.018, 10, "Moderado, 4"),
+    new provincias("Tierra del Fuego", "Parcialmente nublado", 11, 8, 55, 31, 999, 14, "Bajo, 1"),
     new provincias("Tucuman", "Mayormente nublado", 22, 22, 54, 11, 1.020, 10, "Bajo, 2"),
 ]
 
@@ -103,31 +103,68 @@ ese nombre, (siendo que esta misma informacion se va a guardar en el localStorag
 const resultado = provinciasArgentinas.find(x => x.nombre == volverUbicacion)
 console.log(resultado);
 
-//guardo en el localStorage mi arreglo de provincias, donde convierte mi objeto en una cadena de texto JSON
-localStorage.setItem('arregloProvincias', JSON.stringify(provinciasArgentinas));
-let listaProvincias = JSON.parse(localStorage.getItem('arregloProvincias'));
 
 //Para que el "resultado" se muestre en pantalla 
-var vistaA = document.getElementById("vistaA");
-vistaA.innerHTML =`<h1>${resultado.temperatura} Cº</h1>
+const vistaA = document.getElementById("vistaA");
+vistaA.innerHTML = `<h1>${resultado.temperatura} Cº</h1>
                 <h5>Sensacion termica: ${resultado.sensacionTermica} Cº</h5>
                 <h3>${resultado.estado}</h3>
                 <h2>${resultado.nombre}, Argentina</h2>`;
 
-var humedad = document.getElementById("humedad");
-humedad.innerHTML =  `<h5>Humedad: ${resultado.humedad} % </h5>`;
+const humedad = document.getElementById("humedad");
+humedad.innerHTML = `<h5>Humedad: ${resultado.humedad} % </h5>`;
 
-var velocidadViento = document.getElementById("velocidadViento");
-velocidadViento.innerHTML =  `<h5>Velocidad del Viento: ${resultado.velocidadDelViento} km/h </h5>`;
+const velocidadViento = document.getElementById("velocidadViento");
+velocidadViento.innerHTML = `<h5>Velocidad del Viento: ${resultado.velocidadDelViento} km/h </h5>`;
 
-var presionDelAire = document.getElementById("presionAire");
-presionDelAire.innerHTML =  `<h5>Presion del Aire: ${resultado.presionDelAire} mbar </h5>`;
+const presionDelAire = document.getElementById("presionAire");
+presionDelAire.innerHTML = `<h5>Presion del Aire: ${resultado.presionDelAire} mbar </h5>`;
 
-var visibilidad = document.getElementById("visibilidad");
-visibilidad.innerHTML =  `<h5>Visibilidad: ${resultado.visibilidad} km </h5>`;
+const visibilidad = document.getElementById("visibilidad");
+visibilidad.innerHTML = `<h5>Visibilidad: ${resultado.visibilidad} km </h5>`;
 
-var indiceUV = document.getElementById("indiceUV");
-indiceUV.innerHTML =  `<h5>Indice UV: ${resultado.indiceUV} % </h5>`;
+const indiceUV = document.getElementById("indiceUV");
+indiceUV.innerHTML = `<h5>Indice UV: ${resultado.indiceUV} % </h5>`;
+
+const lunes = document.getElementById("lunes");
+lunes.innerHTML = `<img src="images/lluvia2.jpg"  alt="">
+                    <figcaption>
+                        <h3 class="title1">18 Cº</h3>
+                        <h3 class="title2">Lluvioso</h3>
+                        <h3 class="title3">Lunes</h3>
+                    </figcaption><a href="#"></a>`;
+
+var martes = document.getElementById("martes");
+martes.innerHTML = `<img src="images/nublado2.jpg"  alt="">
+                    <figcaption>
+                        <h3 class="title1">24 Cº</h3>
+                        <h3 class="title2">Nublado</h3>
+                        <h3 class="title3">Martes</h3>
+                    </figcaption><a href="#"></a>`;
+
+var miercoles = document.getElementById("miercoles");
+miercoles.innerHTML = `<img src="images/cielo-sol.jpg"  alt="">
+                        <figcaption>
+                            <h3 class="title1">27 Cº</h3>
+                            <h3 class="title2">Soleado</h3>
+                            <h3 class="title3">Miercoles</h3>
+                        </figcaption><a href="#"></a>`;
+
+var jueves = document.getElementById("jueves");
+jueves.innerHTML = `<img src="images/cielo-sol.jpg"  alt="">
+                    <figcaption>
+                        <h3 class="title1">27 Cº</h3>
+                        <h3 class="title2">Soleado</h3>
+                        <h3 class="title3">Jueves</h3>
+                    </figcaption><a href="#"></a>`;
+
+var viernes = document.getElementById("viernes");
+viernes.innerHTML = `<img  src="images/nublado2.jpg"  alt="">
+                    <figcaption>
+                        <h3 class="title1">23 Cº</h3>
+                        <h3 class="title2">Nublado</h3>
+                        <h3 class="title3">Viernes</h3>
+                    </figcaption><a href="#"></a>`
 
 /*Esta funcion permite al usuario una vez elegida su provincia como predeterminda y quiera ver 
 que sucede en las otras, mediente un input se escribe la provincia que se quiera buscar y al hacer click en el boton
@@ -162,9 +199,14 @@ document.getElementById('buscar').addEventListener("click", function () {
         var indiceUV = document.getElementById("indiceUV");
         indiceUV.innerHTML = `<h5>Indice UV: ${city.indiceUV} % </h5>`;
     } else {
-        alert('Esa provincia no se encuentra, por favor, vuela a intentarlo')
-    }
-
+        //alert('Esa provincia no se encuentra, por favor, vuela a intentarlo')
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'La informacion solicitada no se encuentra, por favor, vuela a intentarlo!',
+        })
+    };
+    //aca dentro tambien debe ir la libreria rainyday.js
 });
 
 /*PASOS PARA EJECUTAR Y MOSTRAR UN RELOJ Y FECHA EN TIEMPO REAL
@@ -179,71 +221,54 @@ document.getElementById('buscar').addEventListener("click", function () {
             variables como: dia, mes, hora, mes, etc y arrays
         2do) un numero, son los segundos (1000)*/
 
-setInterval( () => {
-    
+setInterval(() => {
+
     //Variables para la fecha
-    
+
     /* document.getElementsByClassName("...") retorna un objeto similar a un array de los elementos 
     hijos que tengan todos los nombres de clase indicados. Cuando es llamado sobre el objeto document, 
     la busqueda se realiza en todo el document, incluido el nodo raíz
     [0] significa en la posicion 0*/
-    
-        var fecha = document.getElementsByClassName("fecha")[0];
-        var diaSemana = new Date();
-        var diaSem = diaSemana.getDay();
-        var dia = diaSemana.getDate();
-        var mes = diaSemana.getMonth();
-        var year = diaSemana.getFullYear();
+
+    const fecha = document.getElementsByClassName("fecha")[0];
+    const diaSemana = new Date();
+    const diaSem = diaSemana.getDay();
+    const dia = diaSemana.getDate();
+    const mes = diaSemana.getMonth();
+    const year = diaSemana.getFullYear();
 
     //Arrays para la semana y para los meses
-        var semana = ['Domingo','Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
-        var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
-    
+    const semana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
+    const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
     //La propiedad Element.innerHTML devuelve o establece la sintaxis HTML describiendo los descendientes del elemento.
     //Al establecerse se reemplaza la sintaxis HTML del elemento por la nueva.
-        fecha.innerHTML = `${semana[diaSem]} ${dia} de ${meses[mes]} del ${year}`;
+    fecha.innerHTML = `${semana[diaSem]} ${dia} de ${meses[mes]} del ${year}`;
 
     //Variables para el reloj
-        var reloj = document.getElementsByClassName("reloj")[0];
-        var tiempoActual = new Date();
-        var hora = tiempoActual.getHours();
-        var ampm;
+    //OPTIMIZADO
+    let reloj = new Date();
+    let hora = reloj.getHours();
+    let minutos = reloj.getMinutes();
+    let segundos = reloj.getSeconds();
+    var ampm;
     
-    //Creo condicionales 
-    /*Este if lo que hace es transformar el reloj de 24hs a 12hs
-    si pasan de las 12hs significa que estamos en la tarde (pm)
-    sino son las horas de mañana (am)*/
+    if (hora >= 12) {
+        hora = hora - 12;
+        ampm = 'PM';
+    } else {
+        ampm = 'AM';
+    };
 
-            if(hora >= 12 ) {
-                hora = hora - 12;
-                ampm = 'PM';
-            } else {
-                ampm = 'AM';
-            }
+    if (minutos < 10) {
+        minutos = "0" + minutos
+    };
 
-            if(hora == 0) {
-                hora = 12;
-            }
-    
-    /*cuando se este ejecutando el reloj, va a aparecer en los
-    minutos y los segundos un solo numero asi: 10hs 1min 3seg.  bueno, no queremos 
-    que eso pase, queremos que se nos muestre asi: 10hs 01min 06seg.
-    con el 0 delante del minuto y del segundo, y cuando llegue a 10 cambie y no 
-    aparezca, por lo tanto implementamos un if de la siguiente manera*/
+    if (segundos < 10) {
+        segundos = "0" + segundos
+    };
 
-        var minutos = tiempoActual.getMinutes();
-    
-            if (minutos < 10) {
-                minutos = "0" + minutos
-            }; 
-
-        var segundos = tiempoActual.getSeconds();
-        
-            if (segundos < 10) {
-                segundos = "0" + segundos
-            }; 
-
-        reloj.innerHTML = `${hora}:${minutos}:${segundos} ${ampm}`;
+    document.getElementsByClassName("reloj")[0].innerHTML = `${hora}:${minutos}:${segundos} ${ampm}`;
 
 }, 1000);
 
@@ -261,3 +286,151 @@ en la segunda: cuando escuches que se haga "click" en el boton que tiene como id
 */
 entrarButton.addEventListener('click', () => container.classList.add('right-panel-active'));
 volverButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
+
+//Funcion traida de la "Libreria Highcharts"
+Highcharts.chart('container2', {
+
+    title: {
+        text: 'Highcharts Wind Barbs'
+    },
+
+    xAxis: {
+        type: 'datetime',
+        offset: 40
+    },
+
+    plotOptions: {
+        series: {
+            pointStart: Date.UTC(2017, 0, 29),
+            pointInterval: 36e5
+        }
+    },
+
+    series: [{
+        type: 'windbarb',
+        data: [
+            [9.8, 177.9],
+            [10.1, 177.2],
+            [11.3, 179.7],
+            [10.9, 175.5],
+            [9.3, 159.9],
+            [8.8, 159.6],
+            [7.8, 162.6],
+            [5.6, 186.2],
+            [6.8, 146.0],
+            [6.4, 139.9],
+            [3.1, 180.2],
+            [4.3, 177.6],
+            [5.3, 191.8],
+            [6.3, 173.1],
+            [7.7, 140.2],
+            [8.5, 136.1],
+            [9.4, 142.9],
+            [10.0, 140.4],
+            [5.3, 142.1],
+            [3.8, 141.0],
+            [3.3, 116.5],
+            [1.5, 327.5],
+            [0.1, 1.1],
+            [1.2, 11.1]
+        ],
+        name: 'Wind',
+        color: Highcharts.getOptions().colors[1],
+        showInLegend: false,
+        tooltip: {
+            valueSuffix: ' m/s'
+        }
+    }, {
+        type: 'area',
+        keys: ['y', 'rotation'], // rotation is not used here
+        data: [
+            [9.8, 177.9],
+            [10.1, 177.2],
+            [11.3, 179.7],
+            [10.9, 175.5],
+            [9.3, 159.9],
+            [8.8, 159.6],
+            [7.8, 162.6],
+            [5.6, 186.2],
+            [6.8, 146.0],
+            [6.4, 139.9],
+            [3.1, 180.2],
+            [4.3, 177.6],
+            [5.3, 191.8],
+            [6.3, 173.1],
+            [7.7, 140.2],
+            [8.5, 136.1],
+            [9.4, 142.9],
+            [10.0, 140.4],
+            [5.3, 142.1],
+            [3.8, 141.0],
+            [3.3, 116.5],
+            [1.5, 327.5],
+            [0.1, 1.1],
+            [1.2, 11.1]
+        ],
+        color: Highcharts.getOptions().colors[0],
+        fillColor: {
+            linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+            stops: [
+                [0, Highcharts.getOptions().colors[0]],
+                [
+                    1,
+                    Highcharts.color(Highcharts.getOptions().colors[0])
+                        .setOpacity(0.25).get()
+                ]
+            ]
+        },
+        name: 'Wind speed',
+        tooltip: {
+            valueSuffix: ' m/s'
+        },
+        states: {
+            inactive: {
+                opacity: 1
+            }
+        }
+    }]
+
+});
+
+var snippet = [].slice.call(document.querySelectorAll('.hover'));
+if (snippet.length) {
+    snippet.forEach(function (snippet) {
+        snippet.addEventListener('mouseout', function (event) {
+            if (event.target.parentNode.tagName === 'figure') {
+                event.target.parentNode.classList.remove('hover')
+            } else {
+                event.target.parentNode.classList.remove('hover')
+            }
+        });
+    });
+}
+
+/*const lluvioso = provinciasArgentinas.filter(j => j.estado === "Lluvioso");
+const soleado = provinciasArgentinas.filter(l => l.estado === "Soleado");
+console.log(lluvioso);
+console.log(soleado);
+
+function quellueva() {
+    var image = document.getElementById('background');
+    image.onload = function () {
+        var engine = new RainyDay({
+            image: this,
+            blur: 10,
+            opacity: 1,
+            gravityAngle: Math.PI / 2,
+            gravityAngleVariance: 0
+        });
+        engine.gravity = engine.GRAVITY_NON_LINEAR;
+        engine.trail = engine.TRAIL_SMUDGE;
+        engine.rain([
+            [0, 3, 8],
+            [3, 8, 1]
+        ], 50);
+    };
+    image.crossOrigin = 'anonymous';
+    image.src = 'images/bg1.jpg';
+}
+
+quellueva();*/
